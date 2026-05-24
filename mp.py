e7fed3c65343420dbda9cc505745c179026c2a44
Rishita77 <rishita.m8@gmail.com>
2026-05-24 10:51:47 +0530

    Example of usage of Multiprocessing in python

---
+++ mp.py
@@ -0,0 +1,36 @@
+from multiprocessing import Process
+import time
+
+def cpu_heavy():
+    total = 0
+    for i in range(100_000_000):
+        total += i
+
+p1 = Process(target=cpu_heavy)
+p2 = Process(target=cpu_heavy)
+
+start = time.time()
+
+p2.start()
+
+p1.join()
+p2.join()
+
+print("Done:", time.time() - start)
+
+# Each process:
+#
+# has its own Python interpreter,
+# own memory,
+# own GIL.
+#
+# Now true parallelism happens.
+# p1.start()
+
+# But Processes Are Expensive as compared to threads:
+#
+# more memory,
+# IPC overhead,
+# serialization cost.
+#
+# So don't use them for lightweight I/O.
